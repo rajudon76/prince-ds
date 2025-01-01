058a665e4ed99d525755526350d26eae8209b64b
rajudon76 <135225882+rajudon76@users.noreply.github.com>
2025-01-01 22:45:57 +0500

    Update config.js

--- config.js
+++ config.js
@@ -27,9 +27,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 
 //💌global.pairingNumber = "" //put your bot number here💌
-global.mods = ['923071782626'] 
-global.prems = ['923071782626']
-global.allowed = ['923071782626']
+global.mods = ['923295112243'] 
+global.prems = ['923295112243']
+global.allowed = ['923295112243']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
